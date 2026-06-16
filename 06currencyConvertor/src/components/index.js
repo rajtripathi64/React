c9119bc5b95658352792b6a909be860a9d0e0daf
RAJ TRIPATHI <tripathiraj6406@gmail.com>
2026-06-16 13:00:24 +0530

    edit fetch url

--- 06currencyConvertor/src/components/index.js
+++ 06currencyConvertor/src/components/index.js
@@ -1,6 +1,6 @@
 //use for export component if there are to many components file then, import all of them in one palce and then export them , 
 // and when need of them then we access them only by importing one file index.js
 
-import InputBox from "./InputBox"
+import InputBox from "./InputBox";
 
-export {InputBox}
+export {InputBox};
--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,18 +1,38 @@
-import {useEffect, useState} from "react"
+// import {useEffect, useState} from "react"
 
-function useCurrencyInfo(currency){
-    const [data, setData]= useState({})
-    useEffect(()=>{
-        fetch(
-  `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
-)
+// function useCurrencyInfo(currency){
+//     const [data, setData]= useState({})
+//     useEffect(()=>{
+//         fetch(
+//   `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+// )
             
-            .then((res)=>res.json())
-            .then((res)=> setData(res[currency]))
-            console.log(data);
-    }, [currency])
-    console.log(data);
-    return data 
+//             .then((res)=>res.json())
+//             .then((res)=> setData(res[currency]))
+//             console.log(data);
+//     }, [currency])
+//     console.log(data);
+//     return data 
+// }
+
+// export default useCurrencyInfo;
+
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(
+            `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                setData(res[currency]);
+            });
+    }, [currency]);
+
+    return data;
 }
 
 export default useCurrencyInfo;
